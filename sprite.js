5d9061553cefd188ff3a7d9f4e1ffcb55416d0a4
Davichobits <davidruizdeveloper@gmail.com>
2024-01-03 14:37:25 -0500

    Game objects for characters - v3-0:00

--- sprite.js
+++ sprite.js
@@ -1,11 +1,20 @@
 export class Sprite {
   constructor(config){
-
-    // Set up the image
+  
     this.image = new Image();
     this.image.src = config.src;
     this.image.onload = () => {
       this.loaded = true;
+    }
+    
+    // Set up the image
+    this.shadow = new Image();
+    this.useShadow = true; // config.useShadow || false;
+    if(this.useShadow){
+      this.shadow.src = './images/characters/shadow.png';
+    }
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
     }
 
     // Configure Animation & Initial State
@@ -31,7 +40,15 @@ export class Sprite {
     const x = this.gameObject.x * 16 - 8;
     const y = this.gameObject.y * 16 - 18;
 
-    ctx.drawImage(
+    this.isShadowLoaded && ctx.drawImage(
+      this.shadow,
+      0,0,
+      32, 32,
+      x, y,
+      32, 32
+    )
+
+    this.loaded && ctx.drawImage(
       this.image,
       0,0,
       32, 32,
