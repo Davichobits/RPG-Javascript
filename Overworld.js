3a23660acb071d8656efede20423ae66dd948835
Davichobits <davidruizdeveloper@gmail.com>
2024-01-04 14:47:39 -0500

    Game loop created v4-0:00

--- Overworld.js
+++ Overworld.js
@@ -1,33 +1,35 @@
 import { GameObject } from "./GameObject.js";
+import { OverworldMap } from "./OverworldMap.js";
 
 export class Overworld {
   constructor(config) {
       this.element = config.element;
       this.canvas = this.element.querySelector('canvas');
       this.ctx = this.canvas.getContext('2d');
+      this.map = null;
   }
-  init(){
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
+
+  startGameLoop(){
+    const step = () => {
+
+      this.ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+
+      this.map.drawLowerImage(this.ctx);
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.x += 0.02;
+        object.sprite.draw(this.ctx);
+      });
+      this.map.drawUpperImage(this.ctx);
+
+      requestAnimationFrame(()=>step());
     }
-    image.src = '/images/maps/DemoLower.png';
+    step();
+  }
 
-    // Place some Game Objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6,
-    })
+  init(){
+    this.map = new OverworldMap(window.OverworldMap.Kitchen);
+    this.startGameLoop();
 
-    const npc1 = new GameObject({
-      x: 7,
-      y: 8,
-      src: './images/characters/people/npc1.png',
-    })
 
-    setTimeout(()=>{
-      hero.sprite.draw(this.ctx);
-      npc1.sprite.draw(this.ctx);
-    }, 200)
   }
 }
